8850bd5033b420ae60baed19f2d8b4f244a42fd9
hex7c0 <carniellifrancesco@gmail.com>
2014-08-03 12:12:12 +0200

    min with "new line" at EOF

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function(grunt) {
             options: {
                 preserveComments: 'false',
                 banner: '<%= banner %>',
+                footer: '\n'
             },
             target: {
                 files: [{
@@ -47,7 +48,7 @@ module.exports = function(grunt) {
                 failOnError: false
             },
             docs: {
-                command: "jsdoc ./lib/*.js ./module/*.js -c .jsdoc.json"
+                command: 'jsdoc ./lib/*.js ./module/*.js -c .jsdoc.json'
             }
         }
     });
