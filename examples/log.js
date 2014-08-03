61a3d40ab1e72908062e773311d40061f797e333
hex7c0 <carniellifrancesco@gmail.com>
2014-08-03 11:58:28 +0200

    first example

---
+++ examples/log.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ * @file log example
+ * @module logger-request-cli
+ * @package logger-request-cli
+ * @subpackage examples
+ * @version 0.0.2
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/**
+ * initialize module
+ */
+// import
+try {
+    var parser = require('../index.min.js'); // use require('logger-request-cli') instead
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+// using function
+parser({
+    filename: 'A.log',
+    bytesRes: true,
+    bytesReq: true
+});
