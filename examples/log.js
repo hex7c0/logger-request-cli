a1d2bbcdc44f9aeb3e2cb2c788255ed646b966bb
hex7c0 <carniellifrancesco@gmail.com>
2015-03-08 10:26:12 +0100

    build: style fix

--- examples/log.js
+++ examples/log.js
@@ -2,27 +2,20 @@
 /**
  * @file log example
  * @module logger-request-cli
- * @package logger-request-cli
  * @subpackage examples
  * @version 0.0.2
  * @author hex7c0 <hex7c0@gmail.com>
  * @license GPLv3
  */
 
-/**
+/*
  * initialize module
  */
-// import
-try {
-    var parser = require('../index.min.js'); // use require('logger-request-cli') instead
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
+var parser = require('..'); // use require('logger-request-cli') instead
 
 // using function
 parser({
-    filename: 'A.log',
-    // ip: true,
-    response: true
+  filename: 'A.log',
+  // ip: true,
+  response: true
 });
--- examples/search.js
+++ examples/search.js
@@ -2,26 +2,19 @@
 /**
  * @file search example
  * @module logger-request-cli
- * @package logger-request-cli
  * @subpackage examples
  * @version 0.0.2
  * @author hex7c0 <hex7c0@gmail.com>
  * @license GPLv3
  */
 
-/**
+/*
  * initialize module
  */
-// import
-try {
-    var parser = require('../index.min.js'); // use require('logger-request-cli') instead
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
+var parser = require('..'); // use require('logger-request-cli') instead
 
 // using function
 parser({
-    filename: 'A.log',
-    search: 'monitode'
+  filename: 'A.log',
+  search: 'monitode'
 });
